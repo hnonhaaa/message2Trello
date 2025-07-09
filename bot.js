4b53eaff475d8257bb934dd155a69856eaadd46c
peeravee <peeravee@lrks.net>
2025-07-10 05:27:42 +0700

    ♻️ update final working version

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const axios = require('axios');
 
@@ -12,7 +11,6 @@ const client = new Client({
 
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
-
   await axios.post('https://n8n.a8tech.com/webhook/951b0767-7628-4980-8607-872dddd0a2cd', {
     content: message.content,
     author: message.author.username,
