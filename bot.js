9729e440f64f761c792f38fd00d6946fc60881b2
peeravee <peeravee@lrks.net>
2025-07-10 05:18:07 +0700

    ♻️ update final working version

---
+++ bot.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+
+  await axios.post('https://n8n.a8tech.com/webhook/951b0767-7628-4980-8607-872dddd0a2cd', {
+    content: message.content,
+    author: message.author.username,
+    channel: message.channel.name,
+  });
+});
+
+client.login(process.env.DISCORD_TOKEN);
